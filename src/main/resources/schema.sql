f4bbafdf39170700b04a2fe17eba672a837e8d44
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-23 14:25:53 +0100

    Add DDL and DML scripts to initialize database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS students
+(
+    student_id serial PRIMARY KEY,
+    first_name varchar(15) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    group_id integer
+);
+
+CREATE TABLE IF NOT EXISTS courses
+(
+    course_id serial PRIMARY KEY,
+    course_name varchar(50) NOT NULL,
+    course_description varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS groups
+(
+    group_id serial PRIMARY KEY,
+    group_name varchar(25) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS studenttocourse
+(
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INTEGER,
+    course_id INTEGER,
+    CONSTRAINT unique_course UNIQUE (student_id, course_id),
+    CONSTRAINT fk_studenttocourse_course_id FOREIGN KEY (course_id)
+        REFERENCES public.courses (course_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_studenttocourse_student_id FOREIGN KEY (student_id)
+        REFERENCES public.students (student_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
