7612ac02b40ef57b4dcf7623df28346998a58e4d
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-25 16:37:26 +0100

    Add droping tables firstly -Change order of creating the tables -Add constraint, forieng key, reference on group table in student table -Remove schema name from studenttocourse table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,22 @@
+DROP TABLE IF EXISTS studenttocourse;
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE IF NOT EXISTS groups
+(
+    group_id serial PRIMARY KEY,
+    group_name varchar(25) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS students
 (
     student_id serial PRIMARY KEY,
     first_name varchar(15) NOT NULL,
     last_name varchar(20) NOT NULL,
-    group_id integer
+    group_id integer,
+    CONSTRAINT fk_student_group_id FOREIGN KEY (group_id)
+        REFERENCES groups (group_id)
 );
 
 CREATE TABLE IF NOT EXISTS courses
@@ -13,12 +26,6 @@ CREATE TABLE IF NOT EXISTS courses
     course_description varchar(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS groups
-(
-    group_id serial PRIMARY KEY,
-    group_name varchar(25) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS studenttocourse
 (
     enrollment_id SERIAL PRIMARY KEY,
@@ -26,11 +33,11 @@ CREATE TABLE IF NOT EXISTS studenttocourse
     course_id INTEGER,
     CONSTRAINT unique_course UNIQUE (student_id, course_id),
     CONSTRAINT fk_studenttocourse_course_id FOREIGN KEY (course_id)
-        REFERENCES public.courses (course_id) MATCH SIMPLE
+        REFERENCES courses (course_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT fk_studenttocourse_student_id FOREIGN KEY (student_id)
-        REFERENCES public.students (student_id) MATCH SIMPLE
+        REFERENCES students (student_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
