075584aa25667f2e4a6b3bf34f51b553b5790a50
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-26 13:02:35 +0100

    Remove unnecessary TablesDAO interface -Move schema.sql and data.sql files to /src/main/resources/sql/ directory -Add to application.yml current directory of schema.sql and data.sql files

--- src/main/java/ua/foxminded/javaspring/consoleMenu/dao/TablesDAO.java
+++
@@ -1,9 +0,0 @@
-package ua.foxminded.javaspring.consoleMenu.dao;
-
-public interface TablesDAO<T> extends DAO<T> {
-    boolean isTableExist();
-
-    void createTable();
-
-    boolean isTableEmpty();
-}
